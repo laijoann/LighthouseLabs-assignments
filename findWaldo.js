fc17c834832cc6faad7fcef5d88cf11bc52c6645
Joann Lai <lai.joann.cy@gmail.com>
2017-03-09 16:02:35 +0000

    Added findWaldo assignment

---
+++ findWaldo.js
@@ -0,0 +1,16 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+//Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
