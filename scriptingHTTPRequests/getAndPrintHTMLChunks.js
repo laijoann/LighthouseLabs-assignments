cd87fc85fb29a83b9a309acb361b69a9db373ae1
Joann Lai <lai.joann.cy@gmail.com>
2017-03-13 15:34:09 +0000

    Reduced redundancies for getAndPrintHTMLChunks.js

--- scriptingHTTPRequests/getAndPrintHTMLChunks.js
+++ scriptingHTTPRequests/getAndPrintHTMLChunks.js
@@ -1,16 +1,13 @@
 //This first function getAndPrintHTMLChunks will utilize the https library to GET a given URL. This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
 
+const https = require('https');
+
 function getAndPrintHTMLChunks () {
 
-  var requestOptions = {
+  const requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-
-  var https = require('https');
-
-  //utilize https ibrary to GET a given URL
-  //console.log each chunk of data as it is received, concatenated with a newline char so you can visualize each chunk
 
   https.get(requestOptions, (response) => {
     response.setEncoding('utf8');
