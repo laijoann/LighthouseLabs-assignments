469ffaee2b96559d4a3fc21c62ebde8463d4db61
Joann Lai <lai.joann.cy@gmail.com>
2017-03-13 15:17:26 +0000

    Initial commit

---
+++ scriptingHTTPRequests/getAndPrintHTMLChunks.js
@@ -0,0 +1,24 @@
+/*
+Your first function getAndPrintHTMLChunks will utilize the https library to GET a given URL.
+You can use the example above as a reference or template.
+This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
+Remember in this and each subsequent Step - you will always need to invoke (call) the function you wrote.
+
+Considerations:
+Should you require https inside or outside of the function? Does it matter in this case? If not, in what situations would it matter?
+Which of the stream function options ('data', 'error', 'end', 'finish') do you need to use to log the data chunks as they come in?
+What do you notice about the results? Do you think this is the best way to handle the incoming data? How could we improve upon this?
+*/
+
+
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+}
