26bf3f3a6577bd1d2a7f8bfe21256cd501f5d17a
Joann Lai <lai.joann.cy@gmail.com>
2017-03-13 15:31:30 +0000

    Added function for printing in chunks

--- scriptingHTTPRequests/getAndPrintHTMLChunks.js
+++ scriptingHTTPRequests/getAndPrintHTMLChunks.js
@@ -1,16 +1,4 @@
-/*
-Your first function getAndPrintHTMLChunks will utilize the https library to GET a given URL.
-You can use the example above as a reference or template.
-This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
-Remember in this and each subsequent Step - you will always need to invoke (call) the function you wrote.
-
-Considerations:
-Should you require https inside or outside of the function? Does it matter in this case? If not, in what situations would it matter?
-Which of the stream function options ('data', 'error', 'end', 'finish') do you need to use to log the data chunks as they come in?
-What do you notice about the results? Do you think this is the best way to handle the incoming data? How could we improve upon this?
-*/
-
-
+//This first function getAndPrintHTMLChunks will utilize the https library to GET a given URL. This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
 
 function getAndPrintHTMLChunks () {
 
@@ -19,6 +7,21 @@ function getAndPrintHTMLChunks () {
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  var https = require('https');
+
+  //utilize https ibrary to GET a given URL
+  //console.log each chunk of data as it is received, concatenated with a newline char so you can visualize each chunk
+
+  https.get(requestOptions, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (data) => {
+      console.log(data, '\n');
+    })
+    response.on('end', () => {
+      console.log('Response stream complete.')
+    });
+  });
 
 }
+
+getAndPrintHTMLChunks();
