312f53719cf3520dbfd5342f761d33678c62dd2c
Joann Lai <lai.joann.cy@gmail.com>
2017-03-28 15:28:03 +0000

    Cmd line input sanitized

--- famousPeopleScript.js
+++ famousPeopleScript.js
@@ -1,6 +1,5 @@
 const pg = require('pg');
 const settings = require('./settings');
-const argv = process.argv[2];
 
 const client = new pg.Client({
   user     : settings.user,
@@ -11,27 +10,26 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
-dbQuery = () => {
-  return `SELECT * FROM famous_people WHERE first_name = '${argv}' OR last_name = '${argv}'`;
-}
+const argv = process.argv[2];
+const dbQuery = 'SELECT * FROM famous_people WHERE first_name LIKE $1::text OR last_name LIKE $1::text';
 
 dbConnect = () => {
   client.connect((err) => {
     if (err) throw err;
-    client.query(dbQuery(), (err, result) => {
+    client.query(dbQuery, [argv], (err, result) => {
       if (err) throw err;
       console.log('Searching ...');
       data = result.rows;
       client.end();
-      return dbCallback(data);
+      return dbPrint(data);
     })
   })
 }
 
-dbCallback = (data) => {
+dbPrint = (data) => {
   let printStatement = `Found ${data.length} person(s) by the name ${argv}: \n`;
   data.forEach((row) => {
-    printStatement += `- ${row.id}: ${row.first_name} ${row.last_name}, born ${row.birthdate} \n`;
+    printStatement += `- ${row.id}: ${row.first_name} ${row.last_name}, born ${row.birthdate.toDateString()} \n`;
   })
   return console.log(printStatement);
 }
